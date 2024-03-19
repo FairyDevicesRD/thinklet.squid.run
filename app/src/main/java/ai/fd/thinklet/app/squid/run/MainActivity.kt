8055749fac5be2c7498d4f25037301e69ac005c5
Tamaki Hidetsugu <git@ralph.ms>
2024-03-19 23:56:06 +0900

    Use view binding

--- app/src/main/java/ai/fd/thinklet/app/squid/run/MainActivity.kt
+++ app/src/main/java/ai/fd/thinklet/app/squid/run/MainActivity.kt
@@ -1,5 +1,6 @@
 package ai.fd.thinklet.app.squid.run
 
+import ai.fd.thinklet.app.squid.run.databinding.ActivityMainBinding
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,10 +8,15 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy(LazyThreadSafetyMode.NONE) {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
